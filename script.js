6aad05dbb61e73475a27e33e99bf64d1e2ff460a
Patricia <148638762+Patixi@users.noreply.github.com>
2023-10-24 18:20:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector(".submit__button");
+const tyscreen = document.querySelector("#thankyou__screen");
+const ratescreen = document.querySelector("#rate__screen");
+let rate = document.getElementsByClassName("rate__number");
+let valor = undefined;
+
+function submit () { 
+    if (valor !== undefined) {
+        ratescreen.classList.add("hidden");
+        tyscreen.classList.remove("hidden");
+        let frase = document.getElementById("text__rate");
+        frase.innerHTML =`You selected ${valor} out of 5`; 
+    }
+    else {
+      let error = document.querySelector(".error");
+      error.innerHTML = "Upss! You forgot to give us a rating!";
+      return error
+    } 
+};
+
+//Events:
+
+rate[0].addEventListener("click",()=>
+valor = rate[0].innerHTML);
+rate[1].addEventListener("click",()=>
+valor = rate[1].innerHTML);
+rate[2].addEventListener("click",()=>
+valor = rate[2].innerHTML);
+rate[3].addEventListener("click",()=>
+valor = rate[3].innerHTML);
+rate[4].addEventListener("click",()=>
+valor = rate[4].innerHTML);
+
+btn.addEventListener("click",submit);
+
